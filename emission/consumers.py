06f1ff1db58e90cfe0700aba8a8b3d33214ca5ab
Mae Anave Caña <mae.cana@outlook.com>
2023-01-11 23:56:43 +0800

    Send and Receive message from client side - Updated consumers to receive data from client - Updated client to send to consumers

--- emission/consumers.py
+++ emission/consumers.py
@@ -19,14 +19,31 @@ class EmissionConsumer(AsyncWebsocketConsumer):
         )
 
 
-    async def connection_established(self, event):
-        message = event['message']
-
-        await self.send(text_data=json.dumps({'message': message}))
-
-
-    async def disconnect(self):
+    async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.group_space,
             self.channel_name
         )
+
+
+    async def connection_established(self, event):
+        message = event['message']
+
+        await self.send(text_data=json.dumps({'message': message}))
+    
+
+    async def receive(self, text_data):
+        string_data = json.loads(text_data)
+        message = string_data['message']
+
+        await self.channel_layer.group_send(
+            self.group_space,
+            {'type': 'message_received', 'message': message}
+        )
+    
+    
+    async def message_received(self, event):
+        message = event['message']
+
+        await self.send(text_data=json.dumps({'message': message}))
+
